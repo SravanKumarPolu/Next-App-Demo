14aab30f072e74431c8dc9c96629f9bef1cbb058
PVSKR <skr.polu@hotmail.com>
2024-04-10 03:15:48 +0530

    styles modified.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,17 +1,23 @@
 import React from "react";
+import Image from "next/image";
 interface User {
   id: number;
-  name: string;
+  albumId: number;
+  title: string;
+  url: string;
 }
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.com/photos");
   const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
-      <ul>
+      <ul className="grid grid-cols-4">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className="text-center m-1">
+            <p>{user.title}</p>
+            <Image src={user.url} alt={user.title} width={200} height={200} />
+          </li>
         ))}
       </ul>
     </>
