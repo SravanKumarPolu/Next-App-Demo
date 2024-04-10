5f434f60dc6290fd9d21a7c592f1dbedb1f741d8
PVSKR <skr.polu@hotmail.com>
2024-04-10 14:03:13 +0530

    Catching
    how to work without catching  and without catching server-side rendering (static). other wise dynamic

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,20 +2,29 @@ import React from "react";
 import Image from "next/image";
 interface User {
   id: number;
-  albumId: number;
+  title: string;
   url: string;
+  thumbnailUrl: string;
 }
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+  const res = await fetch("https://jsonplaceholder.typicode.com/photos", {
+    next: { revalidate: 10 },
+  });
   const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
-      <ul className="grid grid-cols-4">
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center">
         {users.map((user) => (
           <li key={user.id} className="text-center m-1">
-            {user.albumId}
-            <img src={user.url} />
+            <div className="flex flex-col border rounded p-4">
+              <img
+                src={user.thumbnailUrl}
+                className="mx-auto rounded-full mb-2 w-34 h-34"
+              />
+              <h1 className="text-lg font-semibold ">{user.title}</h1>
+            </div>
           </li>
         ))}
       </ul>
